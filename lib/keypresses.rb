f4ecdff3d2f579fcd805c8d597af8a4aba35f573
Matthiasclee <matthias@matthiasclee.com>
2022-04-19 09:37:53 -0700

    Misc

--- lib/keypresses.rb
+++ lib/keypresses.rb
@@ -1,6 +1,8 @@
 require_relative 'remote.rb'
 require_relative 'webnav.rb'
 
+cmode = :normal
+
 Remote.keypresses do |r|
   r.oncode '00' do |h|
     if h == 0
@@ -8,41 +10,272 @@ Remote.keypresses do |r|
     end
   end
 
-  r.oncode '03' do
-    video = DRIVER.find_element(tag_name: 'video')
-
-    vvol = (video.attribute('volume').to_f*10).to_i
-    vvol = vvol + 1 if vvol != 10
-
-    DRIVER.execute_script("document.getElementsByTagName('video')[0].volume = #{vvol.to_f/10}")
+  r.oncode '01' do |h|
+    if h == 0
+      cmode_a = :kbrd if cmode == :normal
+      cmode_a = :normal if cmode == :kbrd
+      cmode = cmode_a
+    end
   end
 
-  r.oncode '04' do
-    video = DRIVER.find_element(tag_name: 'video')
+  r.oncode '05' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '1' if h != 1
+    end
+  end
 
-    vvol = (video.attribute('volume').to_f*10).to_i
-    vvol = vvol - 1 if vvol != 1
+  r.oncode '06' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '2' if h != 1
+    end
+  end
 
-    DRIVER.execute_script("document.getElementsByTagName('video')[0].volume = #{vvol.to_f/10}")
+  r.oncode '0C' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '3' if h != 1
+    end
+  end
+
+  r.oncode '10' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '4' if h != 1
+    end
+  end
+
+  r.oncode '14' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '5' if h != 1
+    end
+  end
+
+  r.oncode '18' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '6' if h != 1
+    end
+  end
+
+  r.oncode '44' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '9' if h != 1
+    end
+  end
+
+  r.oncode '48' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '0' if h != 1
+    end
+  end
+
+  r.oncode '4C' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys :backspace if h != 1
+    end
+  end
+
+  r.oncode '07' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'q' if h != 1
+    end
+  end
+
+  r.oncode '08' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'w' if h != 1
+    end
+  end
+
+  r.oncode '0B' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'e' if h != 1
+    end
+  end
+
+  r.oncode '0F' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'r' if h != 1
+    end
+  end
+
+  r.oncode '13' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 't' if h != 1
+    end
+  end
+
+  r.oncode '43' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'o' if h != 1
+    end
+  end
+
+  r.oncode '47' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'p' if h != 1
+    end
+  end
+
+  r.oncode '4B' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys :enter if h != 1
+    end
+  end
+
+  r.oncode '0A' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'a' if h != 1
+    end
+  end
+
+  r.oncode '0E' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 's' if h != 1
+    end
+  end
+
+  r.oncode '12' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'd' if h != 1
+    end
+  end
+
+  r.oncode '16' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'f' if h != 1
+    end
+  end
+
+  r.oncode '42' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'j' if h != 1
+    end
+  end
+
+  r.oncode '46' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'k' if h != 1
+    end
+  end
+
+  r.oncode '4A' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'l' if h != 1
+    end
+  end
+
+  r.oncode '09' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'z' if h != 1
+    end
+  end
+
+  r.oncode '0D' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'x' if h != 1
+    end
+  end
+
+  r.oncode '11' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'c' if h != 1
+    end
+  end
+
+  r.oncode '15' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys ' ' if h != 1
+    end
+  end
+
+  r.oncode '19' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'v' if h != 1
+    end
+  end
+
+  r.oncode '1D' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'b' if h != 1
+    end
+  end
+
+  r.oncode '41' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'n' if h != 1
+    end
+  end
+
+  r.oncode '45' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'm' if h != 1
+    end
+  end
+
+  r.oncode '49' do |h|
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '.' if h != 1
+    end
   end
 
   r.oncode '17' do |h|
-    DRIVER.find_element(tag_name: 'body').send_keys :up if h != 1
+    if cmode == :normal
+      DRIVER.find_element(tag_name: 'body').send_keys :up if h != 1
+    end
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'y' if h != 1
+    end
   end
 
   r.oncode '1A' do |h|
-    DRIVER.find_element(tag_name: 'body').send_keys :left if h != 1
+    if cmode == :normal
+      DRIVER.find_element(tag_name: 'body').send_keys :left if h != 1
+    end
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'g' if h != 1
+    end
   end
 
   r.oncode '1C' do |h|
-    DRIVER.find_element(tag_name: 'body').send_keys :right if h != 1
+    if cmode == :normal
+      DRIVER.find_element(tag_name: 'body').send_keys :right if h != 1
+    end
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '7' if h != 1
+    end
   end
 
   r.oncode '1F' do |h|
-    DRIVER.find_element(tag_name: 'body').send_keys :down if h != 1
+    if cmode == :normal
+      DRIVER.find_element(tag_name: 'body').send_keys :down if h != 1
+    end
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'i' if h != 1
+    end
   end
 
   r.oncode '1B' do |h|
-    DRIVER.find_element(tag_name: 'body').send_keys :enter if h == 0
+    if cmode == :normal
+      DRIVER.find_element(tag_name: 'body').send_keys :enter if h == 0
+    end
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'u' if h != 1
+    end
+  end
+
+  r.oncode '1E' do |h|
+    if cmode == :normal
+      DRIVER.find_element(tag_name: 'body').send_keys :escape if h == 0
+    end
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys 'h' if h != 1
+    end
+  end
+
+  r.oncode '40' do |h|
+    if cmode == :normal
+      DRIVER.execute_script("document.getElementsByTagName('body')[0].requestFullscreen()")
+    end
+    if cmode == :kbrd
+      DRIVER.find_element(tag_name: 'body').send_keys '8' if h != 1
+    end
   end
 end
--- lib/webnav.rb
+++ lib/webnav.rb
@@ -8,7 +8,9 @@ profile['general.useragent.override'] = user_agent
 profile['full-screen-api.allow-trusted-requests-only'] = false
 profile['full-screen-api.approval-required'] = false
 profile['full-screen-api.warning.timeout'] = 0
+profile['browser.fullscreen.exit_on_escape'] = false
 options = Selenium::WebDriver::Firefox::Options.new(profile: profile)
+options.add_argument "--kiosk"
 DRIVER = Selenium::WebDriver.for :firefox, options: options
 
 DRIVER.manage.window.full_screen
@@ -17,9 +19,6 @@ module WebNav
   def self.go_to(p)
     DRIVER.navigate.to 'https://youtube.com/tv' if p == :youtube
     DRIVER.navigate.to 'https://youtube.com/tv/activate' if p == :yt_signin
-
-
-    DRIVER.execute_script("document.getElementsByTagName('body')[0].requestFullscreen()")
   end
 end
 
